fffd63a94bc06001a345976cb49059d87d69527a
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-11-19 22:07:16 +0100

    webapp

---
+++ src/main/webapp/precache-manifest.041f8c9ef348d276049ea8cd39023073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76707fba56f4c51fb4aff0db5de88cba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eacb6dcec85ca79d944",
+    "url": "/static/css/main.53b9b4f7.chunk.css"
+  },
+  {
+    "revision": "ba8d6f6acf043ade92c7",
+    "url": "/static/js/2.f12f931e.chunk.js"
+  },
+  {
+    "revision": "4eacb6dcec85ca79d944",
+    "url": "/static/js/main.5e25796f.chunk.js"
+  },
+  {
+    "revision": "6dd8107fa32b57cce2c9",
+    "url": "/static/js/runtime-main.5690d402.js"
+  }
+]);
