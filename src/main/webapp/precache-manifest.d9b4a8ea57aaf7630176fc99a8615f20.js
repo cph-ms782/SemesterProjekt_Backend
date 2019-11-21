f0651adaab64ff131c3b1ed8ea16b9c0f03708e8
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-11-21 13:47:43 +0100

    updating webapp

--- src/main/webapp/precache-manifest.d9b4a8ea57aaf7630176fc99a8615f20.js
+++ src/main/webapp/precache-manifest.d9b4a8ea57aaf7630176fc99a8615f20.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ef419adc38afcfdee81e0cce3aca600",
+    "revision": "fac76fbcd387c825958a08fc39024ffa",
     "url": "./index.html"
   },
   {
-    "revision": "4a761a101c2a7db505f7",
+    "revision": "0ade1ea39796f4d6d5fc",
     "url": "./static/css/main.e4760644.chunk.css"
   },
   {
-    "revision": "0536be0f580b94cb89ef",
-    "url": "./static/js/2.80c6aa4e.chunk.js"
+    "revision": "da27062bdefcaf5f76fa",
+    "url": "./static/js/2.21f30d33.chunk.js"
   },
   {
-    "revision": "4a761a101c2a7db505f7",
-    "url": "./static/js/main.708944fe.chunk.js"
+    "revision": "0ade1ea39796f4d6d5fc",
+    "url": "./static/js/main.d9128ae1.chunk.js"
   },
   {
     "revision": "6c113fa21984866f6957",
