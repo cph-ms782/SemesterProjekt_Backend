17dcadc58a79f87a797c6641ed6afeb0224d6088
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-11-23 23:16:27 +0100

    webapp update

--- src/main/webapp/precache-manifest.0ce057e4ed9ac0066631804c4d8c3abd.js
+++ src/main/webapp/precache-manifest.0ce057e4ed9ac0066631804c4d8c3abd.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "132c24993269387f727a966bf87407d4",
+    "revision": "8cf2e0bbb40c03d5b0c1e947f602d27d",
     "url": "./index.html"
   },
   {
-    "revision": "476a7a7d5d9672422d12",
+    "revision": "40458f5e1d4eae8c76d7",
     "url": "./static/css/main.04c31689.chunk.css"
   },
   {
-    "revision": "c6c44c971cce3db53a84",
-    "url": "./static/js/2.39ee25a9.chunk.js"
+    "revision": "b42cbff6d5db9e61f9c8",
+    "url": "./static/js/2.1a525e5c.chunk.js"
   },
   {
-    "revision": "476a7a7d5d9672422d12",
-    "url": "./static/js/main.ca1accc1.chunk.js"
+    "revision": "40458f5e1d4eae8c76d7",
+    "url": "./static/js/main.77561f90.chunk.js"
   },
   {
     "revision": "6c113fa21984866f6957",
